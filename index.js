c5094f19d4f74abb2efdfbcf9eba46c9c920a37b
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2015-08-03 00:58:26 +0530

    Some More Changes Made

--- index.js
+++ index.js
@@ -15,12 +15,21 @@ function showTweets() {
 	
 	document.getElementsByClassName("current-section")[0].className = document.getElementsByClassName("current-section")[0].className.replace( /(?:^|\s)current-section(?!\S)/g , '' );
 	document.getElementById("cardSection2").className += " current-section";
+	
+	document.getElementById("twitter-widget-0").height = document.getElementById("cardInfoSectionWrapper").clientHeight
+	document.getElementById("twitter-widget-0").width = document.getElementById("cardInfoSectionWrapper").clientWidth
 }
 
 function showBlog() {
+	
+	/*
+	document.getElementById("blogIFrame").src = "http://www.tsjamm.com";
+	
 	document.getElementsByClassName("current-tab")[0].className = document.getElementsByClassName("current-tab")[0].className.replace( /(?:^|\s)current-tab(?!\S)/g , '' );
 	document.getElementById("cardTab3").className += " current-tab";
 	
 	document.getElementsByClassName("current-section")[0].className = document.getElementsByClassName("current-section")[0].className.replace( /(?:^|\s)current-section(?!\S)/g , '' );
 	document.getElementById("cardSection3").className += " current-section";
+	*/
+	window.open("http://www.tsjamm.com",'_target')
 }
